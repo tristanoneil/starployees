d9a07bd5b27e4eeaf43269387b41c572c9a1643e
Tristan O'Neil <tristanoneil@gmail.com>
2018-05-20 14:22:09 -0400

    Link the "logo" to the root path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Employee from './Employee';
 import Employees from './Employees';
 import React, { Component, Fragment } from 'react';
 import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: 'https://us1.prisma.sh/tristan-oneil-3cfa5c/starployees/dev',
@@ -14,16 +14,18 @@ export default class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div className="container mx-auto">
-          <header className="my-8 text-center md:text-left">
-            <h1 className="font-light">
-              <span role="img" aria-label="Star" className="pr-3">
-                ✨
-              </span>
-              Star<span className="text-grey-dark">ployees</span>
-            </h1>
-          </header>
           <Router>
             <Fragment>
+              <header className="my-8 text-center md:text-left">
+                <Link to="/" className="no-underline text-black">
+                  <h1 className="font-light">
+                    <span role="img" aria-label="Star" className="pr-3">
+                      ✨
+                    </span>
+                    Star<span className="text-grey-dark">ployees</span>
+                  </h1>
+                </Link>
+              </header>
               <Route exact path="/" component={Employees} />
               <Route path="/employees/:id" component={Employee} />
             </Fragment>
