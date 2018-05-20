afad47e199d7431893a18c2dd3cfa0912d24f67e
Tristan O'Neil <tristanoneil@gmail.com>
2018-05-20 16:04:00 -0400

    Display more data in the employee detail view

--- src/Employee.js
+++ src/Employee.js
@@ -10,6 +10,9 @@ export const employeeQuery = gql`
       id
       firstName
       lastName
+      phoneNumber
+      email
+      bio
     }
   }
 `;
@@ -18,7 +21,7 @@ export default class Employee extends Component {
   render() {
     return (
       <Fragment>
-        <div className="fixed lg:absolute pin-l pin-b lg:pin-none lg:pin-r lg:pin-t bg-white h-auto lg:h-full w-full lg:w-1/3 p-6 border-grey-light border-t lg:border-0 lg:border-l text-center opacity-75">
+        <div className="fixed lg:absolute pin-l pin-b lg:pin-none lg:pin-r lg:pin-t bg-white h-auto lg:h-full w-full lg:w-1/3 p-6 border-grey-light border-t lg:border-0 lg:border-l opacity-75 shadow">
           <Query
             query={employeeQuery}
             variables={{ id: this.props.match.params.id }}>
@@ -26,9 +29,22 @@ export default class Employee extends Component {
               if (loading) return <p>Loading</p>;
               if (error) return <Error />;
               return (
-                <h2>
-                  {data.employee.firstName} {data.employee.lastName}
-                </h2>
+                <Fragment>
+                  <h2 className="mb-4">
+                    {data.employee.firstName} {data.employee.lastName}
+                  </h2>
+                  <hr className="border-b border-grey-light my-4" />
+                  <p>
+                    <strong>p:</strong> {data.employee.phoneNumber}
+                  </p>
+                  <p>
+                    <strong>e:</strong> {data.employee.email}
+                  </p>
+                  <h4 className="uppercase bg-blue inline-block text-white my-4 px-4 py-2 rounded text-xs">
+                    Bio
+                  </h4>
+                  <p className="leading-normal">{data.employee.bio}</p>
+                </Fragment>
               );
             }}
           </Query>
