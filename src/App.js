b0661668605be2a5456a2b6fcf3377b584171a9a
Tristan O'Neil <tristanoneil@gmail.com>
2018-05-19 21:44:17 -0400

    Setup apollo
    - Install deps
    - Connect to prisma endpoint
    - Wrap application with apollo provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import ApolloClient from 'apollo-boost';
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import Employees from './Employees';
+
+const client = new ApolloClient({
+  uri: 'https://us1.prisma.sh/tristan-oneil-3cfa5c/starployees/dev',
+});
 
 export default class App extends Component {
   render() {
     return (
-      <h1>
-        <marquee>
-          <span role="img" aria-label="Construction Worker">
-            👷
-          </span>
-          &nbsp;Under Construction
-        </marquee>
-      </h1>
+      <ApolloProvider client={client}>
+        <Employees />
+      </ApolloProvider>
     );
   }
 }
