c55a5e46127025a007ec466a8de38fa18cf25848
Tristan O'Neil <tristanoneil@gmail.com>
2018-05-20 14:52:58 -0400

    Implement employee searching

--- src/Employees.js
+++ src/Employees.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
+import qs from 'query-string';
 
 export const employeesQuery = gql`
-  {
-    employees(orderBy: lastName_ASC) {
+  query($query: String!) {
+    employees(orderBy: lastName_ASC, where: { firstName_contains: $query }) {
       id
       firstName
       lastName
@@ -15,42 +16,80 @@ export const employeesQuery = gql`
 `;
 
 export default class Employees extends Component {
+  constructor(props) {
+    super(props);
+
+    const query = window.location.search
+      ? qs.parse(window.location.search).query
+      : '';
+
+    this.state = {
+      query,
+    };
+  }
+
+  handleSearch = event => {
+    const query = event.target.value;
+
+    this.setState({
+      query,
+    });
+
+    this.props.history.push({
+      search: `?query=${query}`,
+    });
+  };
+
   render() {
     return (
-      <Query query={employeesQuery}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading</p>;
-          if (error) return <Error />;
+      <div>
+        <div className="px-3 md:px-0 mb-8">
+          <input
+            type="search"
+            onChange={this.handleSearch}
+            value={this.state.query}
+            className="shadow appearance-none border rounded w-full py-4 px-4 text-grey-darker"
+            placeholder="Search"
+          />
+        </div>
+        <Query query={employeesQuery} variables={{ query: this.state.query }}>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading</p>;
+            if (error) return <Error />;
 
-          return (
-            <div className="flex flex-wrap md:-mx-3">
-              {data.employees.map(({ id, firstName, lastName }) => (
-                <Link
-                  to={`/employees/${id}`}
-                  className="px-3 w-full lg:w-1/2 no-underline text-black"
-                  key={id}>
-                  <div className="rounded shadow-md mb-6 bg-white hover:bg-purple hover:text-white">
-                    <div className="px-6 py-4">
-                      <div className="flex items-center">
-                        <div className="w-16">
-                          <img
-                            alt={lastName}
-                            className="rounded-full border-4 border-grey-light"
-                            src={`https://s3.amazonaws.com/starployees/${firstName.toLowerCase()}.jpg`}
-                          />
-                        </div>
-                        <div className="w-auto pl-4 font-bold text-xl">
-                          {firstName} {lastName}
+            return (
+              <div className="flex flex-wrap md:-mx-3">
+                {data.employees.map(({ id, firstName, lastName }) => (
+                  <Link
+                    to={{
+                      pathname: `/employees/${id}`,
+                      search: window.location.search,
+                    }}
+                    className="px-3 w-full lg:w-1/2 no-underline text-black"
+                    key={id}>
+                    <div className="rounded shadow-md mb-6 bg-white hover:bg-purple hover:text-white">
+                      <div className="px-6 py-4">
+                        <div className="flex items-center">
+                          <div className="w-16">
+                            <img
+                              alt={lastName}
+                              className="rounded-full border-4 border-grey-light"
+                              src={`https://s3.amazonaws.com/starployees/${firstName.toLowerCase()}.jpg`}
+                            />
+                          </div>
+                          <div className="w-auto pl-4 font-bold text-xl">
+                            {firstName} {lastName}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          );
-        }}
-      </Query>
+                  </Link>
+                ))}
+              </div>
+            );
+          }}
+        </Query>
+      </div>
     );
   }
 }
--- src/Employees.test.js
+++ src/Employees.test.js
@@ -9,7 +9,7 @@ import { waitForApollo } from './testHelpers';
 it('renders employees', async () => {
   const mocks = [
     {
-      request: { query: employeesQuery },
+      request: { query: employeesQuery, variables: { query: '' } },
       result: {
         data: {
           employees: [
@@ -36,4 +36,54 @@ it('renders employees', async () => {
   );
 
   expect(component.text()).toMatch('First Last');
+});
+
+describe('searching', () => {
+  let component, pushHistorySpy;
+
+  beforeEach(async () => {
+    const mocks = [
+      {
+        request: { query: employeesQuery, variables: { query: 'Search' } },
+        result: {
+          data: {
+            employees: [
+              {
+                __typename: 'Employee',
+                firstName: 'Search',
+                id: 'id',
+                lastName: 'Query',
+              },
+            ],
+          },
+        },
+      },
+    ];
+
+    pushHistorySpy = jest.fn();
+
+    component = mount(
+      <MockedProvider mocks={mocks}>
+        <MemoryRouter>
+          <Employees history={{ push: pushHistorySpy }} />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+
+    component
+      .find('input[type="search"]')
+      .simulate('change', { target: { value: 'Search' } });
+
+    await waitForApollo(component);
+  });
+
+  it('renders the queried employee', () => {
+    expect(component.text()).toMatch('Search Query');
+  });
+
+  it('updates the browser history with the query', () => {
+    expect(pushHistorySpy).toHaveBeenCalledWith({
+      search: '?query=Search',
+    });
+  });
 });
